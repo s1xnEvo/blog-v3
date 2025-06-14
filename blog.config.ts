4184771f6a6297b710ac8ad979db14008c98eb27
聿雨Ethel <s1xnevo.me@gmail.com>
2025-06-14 17:26:47 +0800

    Update blog.config.ts

--- blog.config.ts
+++ blog.config.ts
@@ -8,14 +8,14 @@ export { zhCN as dateLocale } from 'date-fns/locale/zh-CN'
 // 存储 nuxt.config 和 app.config 共用的配置
 // 此处为启动时需要的配置，启动后可变配置位于 app/app.config.ts
 const blogConfig = {
-    title: '纸鹿摸鱼处',
-    subtitle: '纸鹿至麓不知路，支炉制露不止漉',
+    title: '雾蓝星澈',
+    subtitle: '雾锁蓝舟星澈处，半枕银河半枕书',
     // 长 description 利好于 SEO
-    description: '纸鹿本鹿的个人博客，分享技术与生活。“折腾不止，摸鱼生活——摸门🙏🏻”。纸鹿是一名开源爱好者，结识了许多志同道合的朋友。这个博客记录了他在生活和技术学习中的点滴经历，充满启发与思考。网站界面简洁美观，内容丰富实用，人气互动活跃，涵盖了编程、生活、学习等多个领域，为读者提供了卓越的阅读体验。',
+    description: '蓝斯Van的个人博客，记录生活碎片ing', 
     author: {
-        name: '纸鹿本鹿',
-        avatar: 'https://www.zhilu.cyou/api/avatar.png',
-        email: 'hi@zhilu.cyou',
+        name: '蓝斯Van',
+        avatar: 'https://cdn.jsdelivr.net/gh/Austyncleo/Img_picgo@main/notes/course/avatar.jpg',
+        email: 's1xnevo.me@gmail.com',
         homepage: 'https://www.zhilu.cyou/',
     },
     copyright: {
